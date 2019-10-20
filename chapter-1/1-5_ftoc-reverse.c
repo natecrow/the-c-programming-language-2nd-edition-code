5abd2a6db2cf5277a0fb7619fd62d0ed7a29d2c4
natecrow <nate.crow76@gmail.com>
2019-10-20 14:41:24 -0700

    define symbolic constants

--- chapter-1/1-5_ftoc-reverse.c
+++ chapter-1/1-5_ftoc-reverse.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
+#define LOWER   0   // lower limit of temperature table
+#define UPPER   300 // upper limit
+#define STEP    20  // step size
+
 /** 
  * print Fahrenheit-Celsius table
  */
 int main()
 {
     float fahr, celsius;
-    int lower, upper, step;
-
-    lower = 0;      // lower limit of temperature table
-    upper = 300;    // upper limit
-    step = 20;      // step size
 
     printf("Fahr\tCelsius\n");   // print table heading
-    for (fahr = upper; fahr >= lower; fahr -= step) {
+    for (fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
         celsius = (5.0 / 9.0) * (fahr - 32.0);
         printf("%3.0f\t%3.1f\n", fahr, celsius);
     }
