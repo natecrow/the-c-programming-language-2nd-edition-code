aab546f98a5d2f337841801bc5d86478454400da
natecrow <nate.crow@runbox.com>
2019-12-28 14:14:15 -0500

    Exercises 1-9 and 1-10

---
+++ chapter-1/1-9_replace-blanks.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/* Write a program to copy its input to its output, replacing each string of one or more blanks by a single blank. */
+int main()
+{
+	int c, previousC;
+
+	previousC = EOF; // can really be any nonblank char
+
+	while ((c = getchar()) != EOF) {
+		if (c == ' ') {
+			// only print the first blank in a series
+			if (previousC != ' ') {
+				putchar(c);
+			}
+		}
+		else {
+			putchar(c);
+		}
+
+		previousC = c;
+	}
+
+	return 0;
+}
